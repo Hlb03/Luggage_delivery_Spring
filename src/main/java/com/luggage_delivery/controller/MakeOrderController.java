0881ff588c6f57232f39ff21fcce9cfa3f88d691
Hlb_03 <gfietisov@gmail.com>
2022-12-31 15:27:43 +0200

    update order process

--- src/main/java/com/luggage_delivery/controller/MakeOrderController.java
+++ src/main/java/com/luggage_delivery/controller/MakeOrderController.java
@@ -15,6 +15,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.security.Principal;
+import java.sql.Date;
 
 @Controller
 @RequestMapping("/make-order")
@@ -29,57 +31,71 @@ public class MakeOrderController {
         this.deliveryService = deliveryService;
     }
 
+
     @GetMapping("")
-    public String showOrderPage(@RequestParam(value = "totalPrice", required = false) BigDecimal totalPrice,
+    public String showOrderPage(@RequestParam(value = "totalPrice", required = false) String totalPrice,
                                 @RequestParam(value = "size", required = false) String size,
                                 @RequestParam(value = "type", required = false) String type,
                                 @RequestParam(value = "weight", required = false) String weight,
-                                @RequestParam(value = "deliveryDate", required = false) String date,
-                                @RequestParam(value = "address", required = false) String address,
                                 @RequestParam(value = "routeId", required = false) String routeId,
+                                @RequestParam(value = "address", required = false) String address,
+                                @RequestParam(value = "deliveryDate", required = false) Date deliveryDate,
                                 @RequestParam(value = "option", required = false) String option,
+                                Principal principal,
                                 Model model) {
 
         model.addAttribute("allRoutes", routeService.findRoutes());
 
-        System.out.println("ATTRIBUTES AFTER PRICE CALC: " + totalPrice + " " + size + " " + type + " " + weight);
         model.addAttribute("totalPrice", totalPrice);
         model.addAttribute("size", size);
         model.addAttribute("type", type);
         model.addAttribute("weight", weight);
-        model.addAttribute("deliveryDate", date);
-        model.addAttribute("address", address);
         model.addAttribute("routeId", routeId);
+        model.addAttribute("deliveryDate", deliveryDate);
+        model.addAttribute("address", address);
         model.addAttribute("option", option);
 
         model.addAttribute("delivery", new Delivery());
+        if (principal != null)
+            model.addAttribute("userLogin", principal.getName());
+
         return "make-order";
     }
 
     @PostMapping("/order-process")
-    public String orderProcess(@ModelAttribute("delivery")Delivery delivery,
-                               @RequestParam(value = "option")String option,
+    public String orderProcess(@RequestParam("size") double size,
+                               @RequestParam("type") String type,
+                               @RequestParam("weight") double weight,
+                               @RequestParam("deliveryDate") Date deliveryDate,
+                               @RequestParam("address") String address,
+                               @RequestParam("routeId") int routeId,
+                               @RequestParam(value = "option") String option,
                                Authentication authentication) {
 
         BigDecimal totalPrice = deliveryService.calculateOrderPrice(
-                delivery.getLuggageSize(), delivery.getWeight(), delivery.getRoute().getId(), option);
-        deliveryService.addNewDelivery(delivery.getLuggageSize(), delivery.getWeight(),
-                                       delivery.getLuggageType(), delivery.getAddress(),
-                                       delivery.getDeliveryDate(), delivery.getRoute().getId(),
-                                       totalPrice, authentication.getName());
-        return "redirect:/";
+                size, weight, routeId, option);
+
+        deliveryService.addNewDelivery(size, weight, type, address,
+                deliveryDate, routeId,
+                totalPrice, authentication.getName());
+        return "redirect:/user-order";
     }
 
     @PostMapping("/price-calculate")
-    public String orderPriceCalculate(@ModelAttribute("delivery")Delivery delivery,
-                                      @RequestParam(value = "option")String option){
+    public String orderPriceCalculate(@RequestParam(value = "size") double size,
+                                      @RequestParam(value = "type") String type,
+                                      @RequestParam(value = "weight") double weight,
+                                      @RequestParam(value = "routeId") int routeId,
+                                      @RequestParam(value = "deliveryDate") Date deliveryDate,
+                                      @RequestParam(value = "address") String address,
+                                      @RequestParam(value = "option") String option) {
 
         BigDecimal totalPrice = deliveryService.calculateOrderPrice(
-                delivery.getLuggageSize(), delivery.getWeight(), delivery.getRoute().getId(), option);
+                size, weight, routeId, option);
 
-        return "redirect:/make-order?totalPrice=" + totalPrice + "&size=" + delivery.getLuggageSize() +
-                "&type=" + delivery.getLuggageType() + "&weight=" + delivery.getWeight()
-                + "&deliveryDate=" + delivery.getDeliveryDate() + "&address=" + delivery.getAddress() +
-                "&routeId=" + delivery.getRoute().getId() + "&option=" + option;
+        return "redirect:/make-order?totalPrice=" + totalPrice + "&size=" + size +
+                "&type=" + type + "&weight=" + weight
+                + "&deliveryDate=" + deliveryDate + "&address=" + address +
+                "&routeId=" + routeId + "&option=" + option;
     }
 }
