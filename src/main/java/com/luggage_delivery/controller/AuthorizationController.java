49374ec3ffd9029b280555b6ae462693b0ac6ec3
Hlb_03 <gfietisov@gmail.com>
2022-12-31 15:05:26 +0200

    update authorization controller and view

--- src/main/java/com/luggage_delivery/controller/AuthorizationController.java
+++ src/main/java/com/luggage_delivery/controller/AuthorizationController.java
@@ -6,15 +6,20 @@ package com.luggage_delivery.controller;
 */
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/login")
 public class AuthorizationController {
 
     @GetMapping
-    public String singIn() {
+    public String singIn(@RequestParam(value = "error", required = false) String error,
+                         Model model) {
+        if (error != null)
+            model.addAttribute("error", "Incorrect login/password");
         return "login";
     }
 }
