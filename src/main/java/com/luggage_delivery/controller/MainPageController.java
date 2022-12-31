8312845730e65869f5c77461dcc6ada680056584
Hlb_03 <gfietisov@gmail.com>
2022-12-31 15:26:10 +0200

    update main page representation

--- src/main/java/com/luggage_delivery/controller/MainPageController.java
+++ src/main/java/com/luggage_delivery/controller/MainPageController.java
@@ -1,43 +1,55 @@
 package com.luggage_delivery.controller;
 
+import com.luggage_delivery.entity.Route;
 import com.luggage_delivery.service.RouteService;
 import com.luggage_delivery.service.TariffService;
+import com.luggage_delivery.session_details.UserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import static com.luggage_delivery.util.PageMaxCountUtil.countTotalAmountOfPage;
+import java.security.Principal;
 
 @Controller
 public class MainPageController {
 
     private final TariffService tariffService;
     private final RouteService routeService;
+    private final UserInfo userInfo;
 
     @Autowired
-    public MainPageController(TariffService tariffService, RouteService routeService) {
+    public MainPageController(TariffService tariffService, RouteService routeService, UserInfo userInfo) {
         this.tariffService = tariffService;
         this.routeService = routeService;
+        this.userInfo = userInfo;
     }
 
     @GetMapping("")
     public String getAllTariffs(
-                            @RequestParam(value = "page", defaultValue = "1", required = false) int page,
-                            @RequestParam(value = "routeField", defaultValue = "distance", required = false) String routeField,
-                            @RequestParam(value = "typeOfRouteSort", defaultValue = "desc", required = false) String typeOfRouteSort,
-                            @RequestParam(value = "typeOfTariffSort", defaultValue = "desc", required = false) String typeOfTariffSort,
-                            @RequestParam(value = "tariffField", defaultValue = "price", required = false) String tariffField,
-                                Model model) {
+            @RequestParam(value = "page", defaultValue = "1", required = false) int page,
+            @RequestParam(value = "routeField", defaultValue = "distance", required = false) String routeField,
+            @RequestParam(value = "typeOfRouteSort", defaultValue = "desc", required = false) String typeOfRouteSort,
+            @RequestParam(value = "typeOfTariffSort", defaultValue = "desc", required = false) String typeOfTariffSort,
+            @RequestParam(value = "tariffField", defaultValue = "price", required = false) String tariffField,
+            Principal principal,
+            Model model) {
 
-        model.addAttribute("allRoutes", routeService.findAllRoutes(page, routeField, typeOfRouteSort));
+        Page<Route> routes = routeService.findAllRoutes(page, routeField, typeOfRouteSort);
+        model.addAttribute("allRoutes", routes);
         model.addAttribute("allTariffs", tariffService.getAllTariffs(tariffField, typeOfTariffSort));
+        model.addAttribute("role", userInfo.getUserInfo());
 
-        model.addAttribute("totalPages", countTotalAmountOfPage(routeService.getTotalRoutesAmount()));
+        model.addAttribute("totalPages", routes.getTotalPages());
         model.addAttribute("currentPage", page);
         model.addAttribute("typeOfRouteSort", typeOfRouteSort);
         model.addAttribute("typeOfTariffSort", typeOfTariffSort);
+
+        if (principal != null)
+            model.addAttribute("userLogin", principal.getName());
+
         return "main-page";
     }
 }
